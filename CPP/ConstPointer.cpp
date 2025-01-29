c21317eb90312f23db2e03129767f65f19479d16
jhk63 <163096026+jhk63@users.noreply.github.com>
2025-01-30 01:00:45 +0900

    Update ConstPointer.cpp

--- CPP/ConstPointer.cpp
+++ CPP/ConstPointer.cpp
@@ -5,5 +5,29 @@ using namespace std;
 
 int main()
 {
+	int a = 5;
+	int b = 10;
 
+	// 1. Pointer to a Constant
+	int const * ptr1 = &a;
+
+	// allowed: Changing address
+	ptr1 = &b;
+
+	// not allowed: Trying to change a constant value
+	// *ptr1 = 10;
+
+	cout << *ptr1 << endl;
+
+
+	// 2. Constant Pointer
+	int * const ptr2 = &a;
+
+	// allowed: Changing non-const value
+	*ptr2 = 10;
+
+	// not allowed: Trying to change a constant address
+	// ptr2 = &b;
+
+	cout << *ptr2 << endl;
 }
