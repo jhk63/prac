788d2cfb3c0eda0bb706127b1fc068df38c53726
jhk63 <163096026+jhk63@users.noreply.github.com>
2025-04-05 13:20:56 +0900

    update

--- prac1.cpp
+++ prac1.cpp
@@ -8,21 +8,7 @@ int main()
     int n;
     n = 5;
 
-    int row = (n + 1) / 2;
-
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < (n / 2) - i; j++)
-            cout << " ";
-        
-        for (int j = 0; j < (2 * i) + 1; j++)
-            cout << "*";
-
-        for (int j = 0; j < (n / 2) - i; j++)
-            cout << " ";
-        
-        cout << endl;
-    }
+    cout << (n % 2 ? "SK" : "CY") << endl;
     
     return 0;
 }
